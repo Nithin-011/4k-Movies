5e68ff82cd4655b1d9e8e2e95094b61c43589086
Nithin-011 <97333934+Nithin-011@users.noreply.github.com>
2022-01-08 14:16:41 +0530

    Update translation.py

--- bot/translation.py
+++ bot/translation.py
@@ -47,7 +47,7 @@ For More Details Click Help Button Below..
     
     ABOUT_TEXT = """<b>➥ Name</b> : <code> Auto Filter Bot</code>
     
-<b>➥ Creator</b> : <b><i><a href="https://t.me/AlbertEinstein_TG">AlbertEinstein_TG</a></i></b>
+<b>➥ Creator</b> : <b><i><a href="https://t.me/null_fallaci">4k_movies</a></i></b>
 
 <b>➥ Language</b> : <code>Python3</code>
 
